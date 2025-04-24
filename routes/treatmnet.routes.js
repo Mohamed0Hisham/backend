50223885a680ab79fc79d04e8aa9c169959f0c1c
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-04-25 01:27:51 +0200

    feat: Make show controller for DiseasCategory,Diseases and Treatment controllers

--- routes/treatmnet.routes.js
+++ routes/treatmnet.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
 	index,
+	show,
 	store,
 	update,
 	destroy,
@@ -9,6 +10,7 @@ import authenticateJWT from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get("/", authenticateJWT, index);
+router.get("/:id", authenticateJWT, show);
 router.post("/", authenticateJWT, store);
 router.patch("/:id", authenticateJWT, update);
 router.delete("/:id", authenticateJWT, destroy);
