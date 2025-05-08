e927010759a46e231174a4029997040563cad2b6
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-05-08 15:51:54 +0300

    add: enpoint to print specific diagnosis

--- routes/pdf.routes.js
+++ routes/pdf.routes.js
@@ -10,13 +10,14 @@ const router = express.Router();
 router.get("/diagnosis/:patientId/all", isAuth, async (req, res, next) => {
 	try {
 		const { patientId } = req.params;
-		if (!patientId)
-			return next(errorHandler(400, "missing patient or diagnosis Id"));
+		if (!patientId) return next(errorHandler(400, "missing patient ID"));
 
 		const patient = await User.findById(patientId);
 		if (!patient) return next(errorHandler(404, "patient doesn't exist"));
 
-		const diagnoses = await Diagnosis.find({ patient: patientId });
+		const diagnoses = await Diagnosis.find({ patient: patientId }).populate(
+			"doctor"
+		);
 		if (!diagnoses || diagnoses.length === 0)
 			return next(
 				errorHandler(404, "No diagnoses found for this patient")
@@ -34,5 +35,45 @@ router.get("/diagnosis/:patientId/all", isAuth, async (req, res, next) => {
 		next(errorHandler(500, "internal server error"));
 	}
 });
+
+router.get(
+	"/diagnosis/:patientId/:diagnosisId",
+	isAuth,
+	async (req, res, next) => {
+		try {
+			const { patientId, diagnosisId } = req.params;
+			if (!patientId || !diagnosisId)
+				return next(
+					errorHandler(400, "missing patient or diagnosis ID")
+				);
+
+			const patient = await User.findById(patientId);
+			if (!patient)
+				return next(errorHandler(404, "patient doesn't exist"));
+
+			const diagnosis = await Diagnosis.findbyId(diagnosisId).populate(
+				"doctor"
+			);
+			if (!diagnosis)
+				return next(
+					errorHandler(404, "No diagnosis found for this patient")
+				);
+
+			const PDFDocument = await PDF.generateDiagnosisPDF(
+				patient,
+				diagnosis
+			);
+			res.setHeader("Content-Type", "application/pdf");
+			res.setHeader(
+				"Content-Disposition",
+				`attachment; filename=diagnosis_${Date.now()}.pdf`
+			);
+			PDFDocument.pipe(res);
+		} catch (error) {
+			console.log(error.message);
+			next(errorHandler(500, "internal server error"));
+		}
+	}
+);
 
 export default router;
