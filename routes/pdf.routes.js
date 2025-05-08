6f0f0ba9618d537c163a3ba75330dfd27335b617
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-05-08 14:41:30 +0300

    add: diagnosis endpoint setup

--- routes/pdf.routes.js
+++ routes/pdf.routes.js
@@ -1,6 +1,18 @@
-import express from "express"
+import express from "express";
+import { errorHandler } from "../helpers/errorHandler.js";
+import isAuth from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.get("/diagnosis/:patientId/:diagnosisId?", isAuth ,async (req, res, next) => {
+	try {
+		const { patientId, diagnosisId } = req.params;
+		const showAll = req.query.all === "true";
+
+        const patient = await Patient
+	} catch (error) {
+		next(errorHandler(500, "internal server error"));
+	}
+});
 
 export default router;
