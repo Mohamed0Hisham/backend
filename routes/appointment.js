cba544406e7e2377fa274e9412a8459b86155c6f
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-06-22 22:19:29 +0300

    feat: caching GET requests of appointment works

--- routes/appointment.js
+++ routes/appointment.js
@@ -8,10 +8,11 @@ import {
 	deleteAppointUser,
 } from "../controllers/appointmentController.js";
 import authintication from "../middlewares/auth.js";
+import cache from "../middlewares/cache.js";
 const router = express.Router();
 
-router.get("/", authintication, index);
-router.get("/:id", authintication, show);
+router.get("/", authintication, cache(600), index);
+router.get("/:id", authintication, cache(600), show);
 router.post("/doctor/:id", authintication, store);
 router.patch("/:id", authintication, update);
 router.delete("/user/:id", authintication, deleteAppointUser);
