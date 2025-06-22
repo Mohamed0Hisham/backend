e2757bf97dd2aa28e5a294e41bfd4a946de5d3d1
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-06-22 23:28:07 +0300

    remove: invalid caching of pdf routes

--- routes/pdf.routes.js
+++ routes/pdf.routes.js
@@ -11,14 +11,12 @@ const router = express.Router();
 router.get(
 	"/diagnosis/:patientId/all",
 	isAuth,
-	cacheForUser(600),
 	printAllDiagnoses
 );
 
 router.get(
 	"/diagnosis/:patientId/:diagnosisId",
 	isAuth,
-	cacheForUser(600),
 	printSpecificDiagnosis
 );
 
