db6d1314919baeb9976554458662c259e1d4c105
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-06-25 02:31:51 +0300

    fix: drop caching for messages

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { index, store } from "../controllers/message.js";
 import authintication from "../middlewares/auth.js";
-import cacheForUser from "../middlewares/cacheForUser.js";
 const router = express.Router();
 router.post("/", authintication, store);
-router.get("/:conversationId", authintication, cacheForUser(600), index);
+router.get("/:conversationId", authintication, index);
 export default router;
