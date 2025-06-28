5f50bb0c6fd28dc73e9408b6f75d9d2946d51097
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-06-28 05:48:08 +0300

    modify authenticateJWT middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,32 +1,32 @@
 import jwt from "jsonwebtoken";
-//import { isTokenInBlacklist } from "../controllers/blacklist.js";
 
 const authenticateJWT = (req, res, next) => {
-	const fullToken = req.cookies.accessToken;
-	// const fullToken = req.headers.authorization;
+	const fullToken = req.headers.authorization;
 	if (!fullToken) {
-		return res.status(403).json({ message: "Access denied" });
+		return res.status(401).json({ message: "Access denied" });
 	}
 
-	/**const accessToken = fullToken.split(" ")[1];
+	const accessToken = fullToken.split(" ")[1];
 	if (!accessToken) {
-		return res.status(403).json({ message: "Access denied" });
+		return res.status(401).json({ message: "Access denied" });
 	}
 
-	/**if (isTokenInBlacklist(token)) {
-		return res
-			.status(401)
-			.json({ message: "Token is invalid, please log in again" });
-	}*/
-
-	jwt.verify(fullToken, process.env.ACCESS_TOKEN_SECRET, (err, decodedToken) => {
-		if (err) {
-			return res.status(403).json({ message: "Invalid token" });
-		}
-		req.user = decodedToken;
+	try {
+		const decoded = jwt.verify(
+			accessToken,
+			process.env.ACCESS_TOKEN_SECRET
+		);
+		req.user = decoded;
 		next();
-	});
+	} catch (error) {
+		return res.status(401).json({
+			success: false,
+			message:
+				error.name === "TokenExpiredError"
+					? "Token expired"
+					: "Invalid token",
+		});
+	}
 };
-
 
 export default authenticateJWT;
