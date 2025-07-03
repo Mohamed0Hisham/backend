5fd5458ad2503a2078b0ddcaff7a7250f1b2f79f
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-07-02 23:43:57 -0700

    fix: show total appointment for admin too

--- routes/appointment.js
+++ routes/appointment.js
@@ -12,7 +12,7 @@ import cache from "../middlewares/cache.js";
 
 const router = express.Router();
 
-router.get("/", authintication, cache(600), index);
+router.get("/", authintication, index);
 router.get("/:id", authintication, cache(600), show);
 router.post("/doctor/:id", authintication, store);
 router.patch("/:id", authintication, update);
