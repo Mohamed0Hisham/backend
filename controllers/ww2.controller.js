56cc6a5abbf636339b878e5e6f54e56c7aa1b9f0
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-07-05 14:34:16 -0700

    feat: Something temp

--- controllers/ww2.controller.js
+++ controllers/ww2.controller.js
@@ -3,30 +3,39 @@ import ww2 from "../models/WWII.js";
 export const addWW2Data = async (req, res, next) => {
   try {
     const { country } = req.body;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-    // Check if this IP has already voted
-    const existingVote = await ww2.findOne({ ip });
-    if (existingVote) {
+    // Check for existing vote cookie
+    if (req.cookies.ww2Voted) {
       return res.status(400).json({
-        message: "You have already voted",
-        alreadyVoted: true,
+        success: false,
+        message: "You have already voted!",
+        redirect: false,
       });
     }
 
     if (!country) {
-      return res.status(400).json({ message: "Country is required" });
+      return res.status(400).json({
+        success: false,
+        message: "Country is required",
+        redirect: false,
+      });
     }
 
-    const newWW2Data = await ww2.create({ country, ip });
+    const newWW2Data = await ww2.create({ country });
 
-    // Set cookie as additional check
-    res.cookie("voted", "true", {
+    // Set cookie that expires in 30 days
+    res.cookie("ww2Voted", "true", {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
 
-    return res.status(201).json(newWW2Data);
+    return res.status(201).json({
+      success: true,
+      message: "Vote recorded successfully",
+      redirect:
+        "https://drive.google.com/file/d/1QTtIWpcxFnX7A6-cmQUn66ItuVXQmQw1/view?usp=sharing", // or your success page
+      data: newWW2Data,
+    });
   } catch (error) {
     return next(error);
   }
